e20cf770a3a0c2fa43e92de4017147a248befffc
0Thomas1 <99365833+0Thomas1@users.noreply.github.com>
2024-03-29 15:51:22 -0700

    Add roles and set current user

--- controllers/server.js
+++ controllers/server.js
@@ -7,6 +7,13 @@ const express = require('express');
 const app = express();
 const model = require('./models/model.js'); // the model
 const path = require('path'); // built-in module to manipulate paths
+const roles = {
+    admin: 'admin',
+    user: 'user',
+    guest: 'guest',
+}; // roles
+let currentUser = roles.guest; // current user
+
 
 // setting up view
 app.set('views', path.join(__dirname, 'views'));
@@ -14,7 +21,7 @@ app.set('view engine', 'ejs'); // embedded javascript
 
 // default endpoint
 app.get('/', (req, res) => {
-    
+    res.redirect('/view/index.html');
 });
 
 app.listen(PORT, () => {
